f44d28d9ca2398c5ea7fc13456de626f6e0d103a
Kevin Franklin <kevin@franklindesign.io>
2025-04-09 19:15:04 -0400

    feat: Initial plugin structure and setup

---
+++ initiative-calculator.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Initiative Calculator
+ * Description: Generates tailored service recommendations from form inputs using Gravity Forms.
+ * Version: 1.0
+ * Author: Kevin Franklin — Nomadic Software
+ */
